5e00c4e3899ebd3d907ab7dc47812363a0133fcf
enzole1 <59623999+enzole1@users.noreply.github.com>
2020-02-03 23:01:36 +0100

    Add files via upload

---
+++ lib/gossip.rb
@@ -0,0 +1,27 @@
+require 'csv'
+class Gossip
+
+    attr_accessor :author, :content
+
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+
+    def save 
+        CSV.open("./db/gossip.csv", "ab") do |csv|
+            csv << [@author, @content]       
+        end
+    end
+
+    def self.all
+        all_gossips = []
+
+            CSV.read("./db/gossip.csv").each do |line|
+                all_gossips << Gossip.new(line[0], line[1])
+            end
+            return all_gossips
+    
+        end
+    end
+
